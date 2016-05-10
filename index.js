e83ebe1ca296f4ec96cdb6c02010290420280653
Brooks Van Buren <woofiewilly@gmail.com>
2016-05-10 18:02:38 -0400

    added to encrypt hook

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var app = express();
+var roman = require('roman-encryption.js');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.get('/encrypt', function(req, res) {
-  res.send('hah, not working yet');
+  var data = {
+    number: req.body.number,
+    threshhold: req.body.threshhold,
+    length: 10 // Default length of returned array
+  };
+  if (req.body.length) data.length = req.body.length;
+  var ret;
+  if ((ret = encrypt(data.number, data.threshhold, data.length))) {
+    res.send(ret);
+  } else {
+    res.send('error has occured in encryption');
+  }
+});
+
+app.get('/decrypt', function(req, res) {
+
 });
 
 app.listen(3000, function () {
---
+++ roman-encryption.js
@@ -0,0 +1,11 @@
+
+
+function encrypt(number, threshhold) {
+
+  return [1, 2, 3, 4, 5];
+};
+
+function decrypt(numbers, threshhold) {
+
+
+};
